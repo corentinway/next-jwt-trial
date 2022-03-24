4896be1582e7134844f1f15ba90a432690dabd2e
Corentin <corentinway@gmail.com>
2022-03-24 22:26:51 +0100

    form with browser validation. No service

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,44 @@
 
 import * as Yup from 'yup';
+import { RequiredStringSchema } from 'yup/lib/string';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm} from 'react-hook-form';
 
 function Login() {
+
+    type FormType = {
+        username: RequiredStringSchema<string | undefined>,
+        password: RequiredStringSchema<string | undefined>,
+    }
+
+    const formConstraint : FormType = {
+        username: Yup.string().required('Username is required'),
+        password: Yup.string().required('Password is required')
+    }
+
+    const validationSchema = Yup.object().shape(formConstraint);
+
+    const formOptions = {
+        resolver: yupResolver(validationSchema)
+    };
+
+    type FormValues = {
+        username: string,
+        password: string,
+    };
+
+    const {register, handleSubmit, setError, formState} = useForm<FormValues>(formOptions);
+    const { errors } = formState;
+
+    
+    // see https://react-hook-form.com/api/useform/handlesubmit/
+    function onSubmit({username, password} : FormValues ) {
+        // TODO FIXME
+        // TODO use setError
+        console.log(`sending login data ${username} and ${password}`);
+        return Promise.resolve();
+    }
+
     return (
         <div className="col-md-6 offset-md-3 mt-5">
             Login Page
@@ -12,26 +49,36 @@ function Login() {
             <div className="card">
                 <h4 className="card-header">Login</h4>
                 <div className="card-body">
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-group">
                             <label>UserName</label>
-                            <input name="username"
+                            <input 
                                 type="text"
-                                className=""
+                                {...register('username')}
+                                className={`form-control ${errors.username ? 'is-invalid' : ''}`}
                             />
+                            <div className="invalid-feedback">{errors.username?.message}</div>
                         </div>
                         <div className="form-group">
                             <label>Password</label>
-                            <input name="username"
+                            <input 
                                 type="password"
-                                className=""
+                                {...register('password')}
+                                className={`form-control ${errors.password ? 'is-invalid' : ''}`}
                             />
+                            <div className="invalid-feedback">{errors.password?.message}</div>
                         </div>
                         <button 
+                            disabled={formState.isSubmitting}
                             className="btn btn-primary"
                         >
+                            {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
                             Login
                         </button>
+                        {
+                            errors.apiError &&
+                            <div className='alert alert-danger mt-3 mb-0'>errors.apiError?.message</div>
+                        }
                     </form>
                 </div>
             </div>
