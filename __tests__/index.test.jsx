a5dee8322519fb18d9e9a8a2c2bcfa3134845ae1
Corentin <corentinway@gmail.com>
2022-03-24 23:51:03 +0100

    more tests

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -8,5 +8,17 @@ describe('Login', () => {
         // https://testing-library.com/docs/queries/about/#screen
         const formTitle = container.querySelector('h4');
         expect(formTitle).toBeDefined();
+        expect(formTitle.textContent).toBe('Login');
+
+        const inputs = container.querySelectorAll('form input');
+        expect(inputs.length).toBe(2);
+
+        const loginInput = inputs[0];
+        expect(loginInput.name).toBe('username');
+
+        const passwordInput = inputs[1];
+        expect(passwordInput.name).toBe('password');
     });
+    // TODO check accessibility label -> input
+    // TODO check field validation
 });
